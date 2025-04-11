52ecc523fc4ba5763c2c8c429c1f82bcbbeedc6c
Baaptistee <144432551+Baaptistee@users.noreply.github.com>
2025-04-11 09:59:56 +0200

    finished TD2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,18 @@ import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { provideHttpClient } from '@angular/common/http';
 import { PostService } from './services/post.service';
 import {PostListComponent} from './components/post-list/post-list.component';
+import {PostListItemComponent} from './components/post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    PostListComponent
+    PostListComponent,
+    PostListItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideHttpClient(),
